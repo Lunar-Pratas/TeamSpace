453f0ab37239885e8fd2cf273be003dab11dd7e9
Taynassilva <taynassilva3456@gmail.com>
2025-04-15 09:50:46 -0300

    Update app.js

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+document.getElementById("cta-button").addEventListener("click", function() {
+    alert("Você será redirecionado para mais informações em breve!");
+});
+function mostrarMensagem() {
+    alert('Serviços em breve!');
+  }
