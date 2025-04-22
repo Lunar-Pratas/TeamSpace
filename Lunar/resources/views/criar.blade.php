9d70fcb776c89b60aec563f3a64620a98d05cd66
cassiwizar <masterticole@gmail.com>
2025-04-22 13:41:41 -0300

    1.6

--- Lunar/resources/views/criar.blade.php
+++ Lunar/resources/views/criar.blade.php
@@ -7,6 +7,26 @@
     @vite(['resources/css/criar.css', 'resources/js/criar.js'])
 </head>
 <body>
-     
+     <header>
+    <div class="logo">LOGISTOCK</div>
+    <nav>
+      <a href="#">CADASTRE-SE</a>
+      <a href="#">SOBRE NÓS</a>
+      <a href="#">BENEFÍCIOS</a>
+    </nav>
+  </header>
+    <div class="reto">
+    <div class="top-button">
+      <button class="btn cadastrar">Cadastre seu produto</button>
+    </div>
+
+    <hr>
+
+    <div class="menu">
+      <button class="btn" onclick="">Catálogo</button>
+      <button class="btn">Estoque</button>
+    </div>
+  </div>
+    </div>
 </body>
 </html>
